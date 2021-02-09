1fa850bb3b9615afea59bef91222f8b00bdc0600
Keryanne <47926567+Keryanne@users.noreply.github.com>
2021-02-09 11:21:13 +0100

    Initial commit

---
+++ Application.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Application
+{
+	public void fonctionPrincipale ()
+	{
+		//DEBUT de votre programme
+		Console.WriteLine("Hello World!");
+		//FIN de votre programme
+	}
+
+	//DECLAREZ VOS FONCTIONS EN DESSOUS DE CETTE LIGNE
+
+	/* EXEMPLES DE PROCEDURES ET FONCTIONS :
+	
+		void uneProcedure(int param1, int param2)
+		{
+			Console.WriteLine(param1+param2);
+		{
+		
+		int uneFonction(int param1, int param2)
+		{
+			int resultat = 0;
+			resultat = param1 + param2;
+			return resultat;
+		}
+		
+		
+		*/
+
+	//NE PAS TOUCHER LE CODE EN DESSOUS DE CETTE LIGNE
+}
+
+
